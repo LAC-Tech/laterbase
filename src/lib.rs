baf972bd503258192988b0de2d32bd4dd717a81a
Lewis Campbell <info@lewiscampbell.tech>
2023-07-25 10:37:29 +1200

    using rwlock, matches semantics of the kv store I will probably use

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::BTreeMap;
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 use serde::{de, Serialize};
 use axum::{http, Json, response, Router, routing};
@@ -10,16 +10,20 @@ mod view;
 
 #[derive(Clone)]
 struct AppState<V: db::Event> {
-	dbs: Arc<Mutex<BTreeMap<String, db::Mem<V>>>>
+	dbs: Arc<RwLock<BTreeMap<String, db::Mem<V>>>>
 }
 
 impl<V: db::Event> AppState<V> {
 	fn new() -> Self {
-		Self { dbs: Arc::new(Mutex::new(BTreeMap::new())) }
+		Self { dbs: Arc::new(RwLock::new(BTreeMap::new())) }
 	}
 
-	fn dbs(&self) -> MutexGuard<BTreeMap<String, db::Mem<V>>> {
-		self.dbs.lock().expect("mutex was poisoned")
+	fn read_dbs(&self) -> RwLockReadGuard<BTreeMap<String, db::Mem<V>>> {
+		self.dbs.read().unwrap()
+	}
+
+	fn write_dbs(&self) -> RwLockWriteGuard<BTreeMap<String, db::Mem<V>>> {
+		self.dbs.write().unwrap()
 	}
 }
 
@@ -27,7 +31,7 @@ async fn create_db<V: db::Event>(
 	Path(name): Path<String>,
 	State(state): State<AppState<V>>
 ) -> impl response::IntoResponse {
-	let mut dbs = state.dbs();
+	let mut dbs = state.write_dbs();
 	dbs.insert(name, db::Mem::new());
     http::StatusCode::CREATED
 }
@@ -41,7 +45,7 @@ async fn db_info<E: db::Event>(
 	Path(db_name): Path<String>,
 	State(state): State<AppState<E>>
 ) -> Result<(http::StatusCode, axum::Json<db::Info>), http::StatusCode>  {
-	let dbs = state.dbs();
+	let dbs = state.read_dbs();
 	let db = dbs.get(&db_name).ok_or(http::StatusCode::NOT_FOUND)?;
 	Ok((http::StatusCode::OK, Json(db.info())))
 }
@@ -51,7 +55,7 @@ async fn bulk_read<E: db::Event + Serialize>(
 	Query(BulkRead {keys}): Query<BulkRead>,
 	State(state): State<AppState<E>>
 ) -> Result<axum::Json<Vec<E>>, http::StatusCode> {
-	let dbs = state.dbs();
+	let dbs = state.read_dbs();
     let db = dbs.get(&db_name).ok_or(http::StatusCode::NOT_FOUND)?;
     let events = db.get(&keys).cloned();
     Ok(Json(events.collect()))
@@ -62,7 +66,7 @@ async fn bulk_write<V: db::Event + Serialize + de::DeserializeOwned>(
 	Query(db_name): Query<String>,
     Json(values): Json<Vec<V>>
 ) -> Result<axum::Json<Vec<db::Key>>, http::StatusCode> {
-    let mut dbs = state.dbs();
+    let mut dbs = state.write_dbs();
 	let db = dbs.get_mut(&db_name).ok_or(http::StatusCode::NOT_FOUND)?;
     let new_keys = db.add_local(&values);
     Ok(Json(new_keys))
@@ -86,6 +90,7 @@ mod tests {
 	use tower::ServiceExt; // for `oneshot` and `ready`
 	
 	use axum::body;
+	use axum::http::StatusCode;
 
 	#[tokio::test]
 	async fn can_create_db() {
@@ -99,7 +104,7 @@ mod tests {
 			.body(body::Body::empty())
 			.unwrap();
 		let res = app.ready().await.unwrap().call(req).await.unwrap();
-		assert_eq!(res.status(), http::StatusCode::CREATED);
+		assert_eq!(res.status(), StatusCode::CREATED);
 
 		let req = http::Request::builder()
 			.method("GET")
@@ -107,6 +112,7 @@ mod tests {
 			.body(body::Body::empty())
 			.unwrap();
 		
+		// Thread local heap allocated bytes from a body
 		let res = app.ready().await.unwrap().call(req).await.unwrap();
 		let status = &res.status();
 
@@ -118,9 +124,9 @@ mod tests {
 			"storage_engine": "memory",
 			"n_events": 0
 		});
-		assert_eq!(status, &http::StatusCode::OK);
+		assert_eq!(status, &StatusCode::OK);
 		assert_eq!(actual, expected);
-	
 	}
+
 }
 
