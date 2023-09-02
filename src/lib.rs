518febc6f0c2c35e32c1c0830fdf76469e928b32
Lewis Campbell <info@lewiscampbell.tech>
2023-09-02 20:16:28 +1200

    beginning of an outline of a simulated replica. start figuring out what actually needs to be a trait and what we can make concrete

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
+//! Laterbase is a bi-temporal event store. This means it has two distinc concepts of time: transaction time and valid time.
+
 use std::collections::BTreeMap;
 
 #[cfg(target_endian = "big")]
 compile_error!("I have assumed little endian throughout this codebase");
 
-/**
- * Laterbase is a bi-temporal event store. This means it has two distinct concepts of time:
- * transaction time and valid time.
- */
+#[cfg(not(target_pointer_width = "64"))]
+compile_error!("This code assumes a 64-bit target architecture");
+
 mod time {
 	/// When the database recored an event
 	#[repr(transparent)]
@@ -22,25 +23,97 @@ mod time {
 
 mod clock {
 	pub type Logical = usize;
+	pub type Physical = std::time::SystemTime;
 }
 
 mod event {
 	use super::{clock, time};
 
-	pub trait ID {
-		fn time(&self) -> time::Transaction<clock::Logical>;
+	/**
+	 * IDs must be globally unique and orderable. They should contain within 
+	 * them the physical valid time. This is so clients can generate their own
+	 * IDs.
+	 * 
+	 * TODO: make sure the physical time is not greater than current time.
+	 */
+	pub trait ID: std::cmp::Ord + std::marker::Copy {
+		fn time(&self) -> time::Valid<clock::Physical>;
 	}
-	pub trait Val {}
+	pub trait Val: std::marker::Copy {}
 }
 
+trait Address: std::marker::Copy {}
+
 /** All of these must be idempotent */
-#[derive(
-	rkyv::Archive, rkyv::Deserialize, rkyv::Serialize, Debug, PartialEq,
-)]
-enum Message<'a, ID: event::ID, E: event::Val> {
-	SendBackTheseEvents(&'a [ID]),
-	SendBackEventsSince(time::Transaction<clock::Logical>),
+#[derive(rkyv::Archive, rkyv::Deserialize, rkyv::Serialize, Debug)]
+enum Message<'a, Addr: Address, ID: event::ID, E: event::Val> {
+	// TODO: send them back where?
+	SendBackTheseEvents((Addr, &'a [ID])),
+	SendBackEventsSince((Addr, time::Transaction<clock::Logical>)),
 	StoreEvents(BTreeMap<ID, E>),
+}
+
+/** 
+ * The mysterious substances that exists between replicas..
+ * HTTP network? Unix sockets? Who knows!
+ */ 
+trait Ether<Addr: Address, EventID: event::ID, Event: event::Val> {
+	fn send(&mut self, addr: Addr, msgs: &[Message<Addr, EventID, Event>]);
+}
+
+/**
+ * A replica is not only a database, but an implementation of an Actor.
+ */
+mod replica {
+	use super::{event, Message, Ether, Address};
+	use std::collections::BTreeMap;
+
+	/// TODO: Assuming all replicas communicate over the same protocol
+	#[derive(Debug)]
+	struct Replica<Addr: Address, ID: event::ID, E: event::Val> {
+		addr: Addr,
+		events: BTreeMap<ID, E>,
+		/// Events added in local append order
+		append_log: Vec<ID>,
+	}
+
+	impl<Addr: Address, ID: event::ID, E: event::Val> Replica<Addr, ID, E> {
+		fn new(addr: Addr) -> Self {
+			let events = BTreeMap::new();
+			let append_log = vec![];
+			Self { addr, events, append_log }
+		}
+
+		fn send<Eth: Ether<Addr, ID, E>>(
+			&mut self,
+			eth: &mut Eth, 
+			msgs: &[Message<Addr, ID, E>]
+		) {
+			for msg in msgs {
+				match msg {
+					Message::SendBackTheseEvents((addr, ids)) => {
+						let events_to_send: BTreeMap<ID, E> = ids
+							.into_iter()
+							.filter_map(|id| {
+								self.events.get(id).map(|e| (*id, *e))
+							})
+							.collect();
+
+						let msgs = &[Message::StoreEvents(events_to_send)];
+
+						eth.send(*addr, msgs);
+					}
+					Message::StoreEvents(events) => {
+						for (id, val) in events {
+							// TODO: super inefficient copying in a loop
+							self.events.insert(*id, *val);
+						}
+					}
+					_ => panic!("oh no!"),
+				}
+			}
+		}
+	}
 }
 
 //mod db;
