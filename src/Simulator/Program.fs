2c1f882e42fe7037db18d0355086c47422d51b1c
Lewis Campbell <info@lewiscampbell.tech>
2023-09-06 08:25:09 +1200

    handling CLi args to get seed

--- src/Simulator/Program.fs
+++ src/Simulator/Program.fs
@@ -1,25 +1,38 @@
 ﻿open System
 open System.Collections.Generic
+open System.Diagnostics
 
-let seed = Random().Next()
+let log (s: string) = printf $"{s}"
 
 type AddressFactory<'e>(seed: int) = 
-  let Rng = Random seed
-  let ether = SortedDictionary<Guid, Library.Replica<'e>>()
-  member _.Create() = 
-      let randomBytes = Array.zeroCreate<byte> 16
-      Rng.NextBytes randomBytes
+    let Rng = Random seed
+    let ether = SortedDictionary<Guid, Library.Replica<'e>>()
+    member _.Create() = 
+        let randomBytes = Array.zeroCreate<byte> 16
+        Rng.NextBytes randomBytes
 
-      let eventId = Guid randomBytes
-      { new Library.IAddress<'e> with
-          member _.Send msg = 
-              match ether |> Library.dictGet eventId with
-              | Some(replica) -> replica.Send msg
-              | None -> 
-                  fprintfn Console.Error "no replica for %A" eventId }
-                  
+        let eventId = Guid randomBytes
+        { new Library.IAddress<'e> with
+            member _.Send msg = 
+                match ether |> Library.dictGet eventId with
+                | Some(replica) -> replica.Send msg
+                | None -> 
+                    fprintfn Console.Error "no replica for %A" eventId }
 type Event = int32
-let addressFactory = AddressFactory<Event>(seed)
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+[<EntryPoint>]
+let main args =
+    let seed = 
+        match args with
+        | [||] -> Random().Next()
+        | [|s|] -> 
+            try
+                int s
+            with | :? System.FormatException as ex ->
+                failwithf "First argument %A was not an integer" s
+
+        | _ -> failwith "too many args"
+
+    let addressFactory = AddressFactory<Event>(seed)
+
+    0
