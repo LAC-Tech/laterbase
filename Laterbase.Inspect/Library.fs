92d968c550816158e22843d534b8c8e7984e8b00
Lewis Campbell <info@lewiscampbell.tech>
2023-09-26 11:05:41 +1300

    moved gui stuff into laterbase.inspect. things compile but do not work

--- Laterbase.Inspect/Library.fs
+++ Laterbase.Inspect/Library.fs
@@ -1,5 +1,71 @@
 ﻿namespace Laterbase.Inspect
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+open Terminal.Gui
+open Laterbase.Core
+
+type Replica<'e>(replica: IReplica<'e>) =
+    inherit Window(
+        "Laterbase Inspector",
+        X = 0,
+        Y = 0,
+        Width = Dim.Fill(),
+        Height = Dim.Fill()
+    )
+
+    do 
+        let events = replica.Read({ByTime = PhysicalValid; Limit = 0uy})
+
+        let tabs = new TabView(
+            X = 0,
+            Y = 0,
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        )
+
+        let eventsDt = new Data.DataTable()
+        eventsDt.Columns.Add "ID" |> ignore
+        eventsDt.Columns.Add "Value" |> ignore
+
+        for (k, v) in events do
+            eventsDt.Rows.Add(k, v) |> ignore
+
+        let eventsView = new TableView(
+            X = 0,
+            Y = 0,
+            Width = Dim.Fill(),
+            Height = Dim.Fill(),
+            Table = eventsDt
+        )
+
+        tabs.AddTab(TabView.Tab("Events", eventsView), true)
+
+        replica.Debug() |> Option.iter (fun viewData ->
+            let appendLogView = new ListView(
+                viewData.AppendLog |> Seq.toArray,
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            )
+
+            let logicalClockDt = new Data.DataTable()
+            logicalClockDt.Columns.Add "Address" |> ignore
+            logicalClockDt.Columns.Add "Sent" |> ignore
+            logicalClockDt.Columns.Add "Received" |> ignore
+            for (addr, sent, received) in viewData.LogicalClock do
+                logicalClockDt.Rows.Add(addr, sent, received) |> ignore
+
+            let logicalClockView = new TableView(
+                logicalClockDt,
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            )
+
+            tabs.AddTab(TabView.Tab("AppendLog", appendLogView), false)
+            tabs.AddTab(TabView.Tab("LogicalClock", logicalClockView), false)
+        )
+
+        base.Add tabs
--- Laterbase.Simulator/Program.fs
+++ Laterbase.Simulator/Program.fs
@@ -1,154 +1,13 @@
 ﻿open System
 open Laterbase
 open Laterbase.Core
-open Terminal.Gui
 
 /// Deterministic Simulation Tester for Laterbase
 /// Inspired by Tigerbeetle Simulator, as well as Will Wilsons talk.
 
-let speedMenu = 
-    seq {"Pause"; "Turtle"; "Llama"; "Cheetah"; "Tiger Beetle"} 
-    |> Seq.map (fun title -> MenuItem (Title = title))
-    |> Seq.toArray
-    |> fun items -> MenuBarItem("Speed", items)
+let addrLen = 16 // TODO: arbitrary
 
-let menuBar = new MenuBar [|speedMenu|]
-
-type Replica(x: int, y: int) =
-    inherit View(x, y, "ᚠ")
-
-let map = new Window(
-    "Laterbase",
-    X = 0,
-    Y = 1,
-    Width = Dim.Fill(),
-    Height = Dim.Fill()
-)
-
-type ExampleWindow() =
-    inherit Toplevel(
-        X = 0,
-        Y = 0,
-        Width = Dim.Fill(),
-        Height = Dim.Fill()
-    )
-
-    let replica1 = new Replica(10, 10)
-    let replica2 = new Replica(20, 10)
-    
-    do
-        map.Add replica1
-        map.Add replica2
-        base.Add map
-        base.Add menuBar
-        
-        base.add_KeyPress(fun args ->
-            match args.KeyEvent.Key with
-            | Key.Esc -> Application.RequestStop ()
-            | _ -> ()
-        )
-
-let mainLoop () =
-    Application.Init()
-    let runState = ref (Application.Begin (new ExampleWindow()))
-    
-    let firstIteration = ref true
-    runState.Value.Toplevel.Running <- true
-
-    let mutable time = 0L<ms>
-
-    let rec loop () =
-        let breakUiLoop =
-            (not runState.Value.Toplevel.Running) ||
-            (Application.ExitRunLoopAfterFirstIteration &&
-            not firstIteration.Value)
-
-        if breakUiLoop then
-            ()
-        else 
-            Application.RunMainLoopIteration(runState, false, firstIteration)
-
-            let dateTime = 
-                DateTimeOffset.FromUnixTimeMilliseconds(int64 time).DateTime
-
-            map.Title <- dateTime.ToString()
-            map.SetNeedsDisplay()
-            time <- time + 10L<ms>
-            loop ()
-
-    loop ()
-
-type ReplicaInspector<'e>(replica: IReplica<'e>) =
-    inherit Window(
-        "Laterbase Inspector",
-        X = 0,
-        Y = 0,
-        Width = Dim.Fill(),
-        Height = Dim.Fill()
-    )
-
-    do 
-        let events = replica.Read({ByTime = PhysicalValid; Limit = 0uy})
-
-        let tabs = new TabView(
-            X = 0,
-            Y = 0,
-            Width = Dim.Fill(),
-            Height = Dim.Fill()
-        )
-
-        let eventsDt = new Data.DataTable()
-        eventsDt.Columns.Add "ID" |> ignore
-        eventsDt.Columns.Add "Value" |> ignore
-
-        for (k, v) in events do
-            eventsDt.Rows.Add(k, v) |> ignore
-
-        let eventsView = new TableView(
-            X = 0,
-            Y = 0,
-            Width = Dim.Fill(),
-            Height = Dim.Fill(),
-            Table = eventsDt
-        )
-
-        tabs.AddTab(TabView.Tab("Events", eventsView), true)
-
-        replica.Debug() |> Option.iter (fun viewData ->
-            let appendLogView = new ListView(
-                viewData.AppendLog |> Seq.toArray,
-                X = 0,
-                Y = 0,
-                Width = Dim.Fill(),
-                Height = Dim.Fill()
-            )
-
-            let logicalClockDt = new Data.DataTable()
-            logicalClockDt.Columns.Add "Address" |> ignore
-            logicalClockDt.Columns.Add "Sent" |> ignore
-            logicalClockDt.Columns.Add "Received" |> ignore
-            for (addr, sent, received) in viewData.LogicalClock do
-                logicalClockDt.Rows.Add(addr, sent, received) |> ignore
-
-            let logicalClockView = new TableView(
-                logicalClockDt,
-                X = 0,
-                Y = 0,
-                Width = Dim.Fill(),
-                Height = Dim.Fill()
-            )
-
-            tabs.AddTab(TabView.Tab("AppendLog", appendLogView), false)
-            tabs.AddTab(TabView.Tab("LogicalClock", logicalClockView), false)
-        )
-
-
-
-        base.Add tabs
-
-// TODO: arbitrary
-let addrLen = 16
-
+/// TODO: move to simulated?
 let randAddr (rng: Random) =
     let bytes = Array.zeroCreate<byte> addrLen
     rng.NextBytes bytes
@@ -182,19 +41,7 @@ let main args =
 
     replicas[0].Recv (StoreNew newEvents)    
     
-    Application.Init()
-
-    let inspector = new ReplicaInspector<string>(replicas[0])
-    // TODO: there should be some standard keypress to stop this?
-    Application.Top.add_KeyPress(fun args ->
-        match args.KeyEvent.Key with
-        | Key.Esc -> Application.RequestStop ()
-        | _ -> ()
-    )
-
-    Application.Top.Add (inspector)
-    Application.Run()
-    Application.Shutdown()
+    let inspector = new Inspect.Replica<string>(replicas[0])
 
     0
 
