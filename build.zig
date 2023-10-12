4c6dd85a110d14ea8377f8903b2c4d08345bfb0d
Lewis Campbell <info@lewiscampbell.tech>
2023-10-12 14:43:06 +1300

    beginning zig re-write

---
+++ build.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+
+// fn linkDeps(c: *std.build.Compile) void {
+//     c.linkLibC();
+//     c.linkSystemLibrary("lmdb");
+// }
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "lb",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.linkLibC();
+    exe.linkSystemLibrary("lmdb");
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    tests.linkLibC();
+    tests.linkSystemLibrary("lmdb");
+
+    const run_tests = b.addRunArtifact(tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_tests.step);
+}
