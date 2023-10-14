e257d904a4b811a8f0dad3f23f57d9f3ee1c6a87
Lewis Campbell <info@lewiscampbell.tech>
2023-10-15 12:42:00 +1300

    basic outline of iterator for bst, but has trouble iterating

--- src/main.zig
+++ src/main.zig
@@ -28,22 +28,61 @@ fn BST(comptime K: type, comptime V: type) type {
         len: usize,
 
         const Self = @This();
-
-        // const Entry = struct { key_ptr: *K, value_ptr: *V };
-        // _ = Entry;
+        const Entry = struct { key_ptr: *K, value_ptr: *V };
+        const ParentStack = std.ArrayListUnmanaged(?*Node);
 
         const Iterator = struct {
-            node: ?*Node,
+            parent_stack: ParentStack,
+            current: ?*Node,
 
-            pub fn init(bst: Self) @This() {
-                var node: ?*Node = null;
-                node = bst.root;
-                if (node) |n| {
-                    while (n.*.left) |left| {
-                        node = left;
+            pub fn init(bst: Self, allocator: std.mem.Allocator) !@This() {
+                var parent_stack =
+                    try ParentStack.initCapacity(allocator, bst.len);
+                var current = bst.root;
+
+                for (0..bst.len) |_| {
+                    if (current) |c| {
+                        if (c.left) |left| {
+                            try parent_stack.append(allocator, current);
+                            current = left;
+                        }
+                    } else {
+                        break;
                     }
                 }
-                return .{ .node = node };
+
+                return .{
+                    .parent_stack = parent_stack,
+                    .current = current,
+                };
+            }
+
+            pub fn deinit(self: *@This(), allocator: std.mem.Allocator) void {
+                self.parent_stack.deinit(allocator);
+            }
+
+            fn next_node(self: *@This()) ?*Node {
+                if (self.current) |current| {
+                    if (current.right == null) {
+                        if (self.parent_stack.popOrNull()) |result| {
+                            return result;
+                        } else {
+                            return null;
+                        }
+                    } else {
+                        return current.right;
+                    }
+                } else {
+                    return null;
+                }
+            }
+
+            pub fn next(self: *@This()) ?Entry {
+                if (self.next_node()) |n| {
+                    return .{ .key_ptr = &n.key, .value_ptr = &n.val };
+                } else {
+                    return null;
+                }
             }
         };
 
@@ -117,8 +156,8 @@ fn BST(comptime K: type, comptime V: type) type {
             return null;
         }
 
-        fn iterator(self: @This()) Iterator {
-            return Iterator.init(self);
+        fn iterator(self: @This(), allocator: std.mem.Allocator) !Iterator {
+            return try Iterator.init(self, allocator);
         }
     };
 }
@@ -131,11 +170,13 @@ test "BST" {
     try std.testing.expectEqual(@as(usize, 1), bst.len);
     try std.testing.expectEqual(@as(?u64, 2), bst.get(4));
     try std.testing.expectEqual(@as(?u64, null), bst.get(27));
+    try bst.put(arena.allocator(), 0, 1);
+    try bst.put(arena.allocator(), 10, 0);
 
-    //try bst.put(arena.allocator(), 0, 1);
-    //var iterator = bst.iterator();
-    //_ = iterator;
+    var iter = try bst.iterator(arena.allocator());
+    defer iter.deinit(arena.allocator());
 
-    // try std.testing.expectEqual(@as(*const u64, &0), iterator.next().?.key_ptr);
-    // try std.testing.expectEqual(@as(*const u64, &4), iterator.next().?.key_ptr);
+    while (iter.next()) |entry| {
+        std.debug.print("key = {}, val = {}", .{ entry.key_ptr.*, entry.value_ptr.* });
+    }
 }
