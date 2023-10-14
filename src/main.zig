59aa4e0b8270a1313dd0d991bc3ee0e58d762ed8
Lewis Campbell <info@lewiscampbell.tech>
2023-10-14 16:33:44 +1300

    outline of binary search tree

--- src/main.zig
+++ src/main.zig
@@ -3,3 +3,28 @@ const std = @import("std");
 pub fn main() void {
     std.debug.print("gettin ziggy wit it", .{});
 }
+
+// Binary Search Tree
+// Implementing this because Zig doesn't have kind of sorted map
+// Also, this is the simplest one I could think of
+// Plan to use this for indices
+fn BST(comptime K: type, comptime V: type) type {
+    const Node = struct {
+        key: K,
+        val: V,
+        left: *?@This(),
+        right: *?@This(),
+    };
+
+    return struct {
+        root: ?*Node,
+
+        fn init() @This() {
+            return .{ .root = null };
+        }
+    };
+}
+
+test "BST" {
+    _ = BST(u64, u64).init();
+}
